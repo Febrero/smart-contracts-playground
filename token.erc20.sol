77c0ef90a1b6ef6c2bea9d4e4d9e2a271849a51e
Pedrocas <pedro.febrero@bityond.com>
2018-04-16 12:52:18 +0100

    Update token.erc20.sol

--- token.erc20.sol
+++ token.erc20.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.10;
+
 interface ERC20 {
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
